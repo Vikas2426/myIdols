b30c78ac0e8d6e58dfb491e510aa610d31fffe5f
Vikas2426 <vikas.cby@gmail.com>
2021-02-28 15:45:10 +0530

    Updates

---
+++ precache-manifest.094af02096ab7e74e6779b1ce492dc79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb41ec0b0af276ba84ddd3947d6c141",
+    "url": "/myIdols/index.html"
+  },
+  {
+    "revision": "ed143c2e17120c355fff",
+    "url": "/myIdols/static/css/main.ee9e915c.chunk.css"
+  },
+  {
+    "revision": "1e06487edb95bdb3a8eb",
+    "url": "/myIdols/static/js/2.cf142384.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/myIdols/static/js/2.cf142384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed143c2e17120c355fff",
+    "url": "/myIdols/static/js/main.1bc2565d.chunk.js"
+  },
+  {
+    "revision": "d18ead6f4df5709dc3fb",
+    "url": "/myIdols/static/js/runtime-main.861e5217.js"
+  }
+]);
